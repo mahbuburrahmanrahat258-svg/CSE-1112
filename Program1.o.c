5d01de5209e5d65f2d142e1ec3a57e9f3b3d4695
Rahat_404 <mahbuburrahmanrahat258@gmail.com>
2026-09-10 20:51:36 +0600

    program1.0

---
+++ Program1.o.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int A,B,Sum ;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &A, &B);
+
+    Sum = A + B;
+
+    printf("Sum = %d", Sum);
+
+    return 0;
+
+}
+
